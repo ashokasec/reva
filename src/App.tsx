67d9eed1a02996d6b15d9e6fef1efa8314c36dd6
Shivam Gupta <ashokasec@proton.me>
2024-07-01 19:24:09 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App () {
+  return (
+    <>
+    <h1 className="text-9xl font-bold absolute bottom-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 leading-1">Reva</h1>
+    </>
+  )
+}
