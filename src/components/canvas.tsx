65be5d2f2c52dac4db43709bb753db2f74cafce6
Shivam Gupta <ashokasec@proton.me>
2024-07-11 23:52:54 +0530

    add: external images

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -33,7 +33,7 @@ const OverlayCanvas = () => {
                     outerContext.drawImage(outerImage, 0, 0, 1080, 720);
 
                     const innerImage = new Image();
-                    innerImage.src = background; // Replace with the path to your inner image
+                    innerImage.src = "https://images.unsplash.com/photo-1469474968028-56623f02e42e?q=80&w=2674&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"; // Replace with the path to your inner image
                     innerImage.onload = () => {
                         innerContext.drawImage(innerImage, 0, 0, innerCanvas.width, innerCanvas.height);
                         innerContext.translate(200, 400);
